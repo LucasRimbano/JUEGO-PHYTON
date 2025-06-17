5f3e41f315f8c0c1ca1b268f041c1b59250a8eb9
Lucas Rimbano <Lucasrimbano@gmail.com>
2025-06-16 23:53:02 -0300

    ajustando pantalla de fondo

--- nuevo_juego/motor.py
+++ nuevo_juego/motor.py
@@ -4,11 +4,16 @@ pygame.init()
 
 # Imagen de fondo
 try:
-    fondo = pygame.image.load('imagenes/fondo_juego.png')
+    fondo = pygame.image.load('nuevo_juego//imagenes//fondo_juego.png')
 except pygame.error:
     print("Error al cargar la imagen. Verifica la ruta.")
     fondo = pygame.Surface((500, 500)) 
+#quiero que me pantalla seamas grande que la imagen de fondo
+    fondo.fill((0, 0, 0))
 
+# Si la imagen de fondo es más pequeña que la pantalla, la escalamos
+if fondo.get_width() < 500 or fondo.get_height() < 500:
+    fondo = pygame.transform.scale(fondo, (1000, 600))       
 # Configuracion de pantalla
 width = fondo.get_width()
 height = fondo.get_height()
