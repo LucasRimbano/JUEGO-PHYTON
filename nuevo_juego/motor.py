7a1feb958e99817a77efb8d3754cfe95b2c2ec63
Lucas Rimbano <Lucasrimbano@gmail.com>
2025-06-26 16:25:06 -0300

    Cambio de fuente y resolucion de problemas

--- nuevo_juego/motor.py
+++ nuevo_juego/motor.py
@@ -20,15 +20,28 @@ def disparar():
     
 def menu_controles():
     screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+    try:
+        fondo_imagen = pygame.image.load('nuevo_juego/imagenes/fondo_princ.jpg')
+        fondo_imagen = pygame.transform.scale(fondo_imagen, (screen.get_width(), screen.get_height()))
+    except pygame.error as e:
+        print(f"No se pudo cargar la imagen: {e}")
+        fondo_imagen = None  
+    if fondo_imagen:
+        screen.blit(fondo_imagen, (0, 0))
+    else:
+        screen.fill((0, 0, 0))
 
     
     pygame.display.set_caption("Seleccioná los controles")
-    font = pygame.font.SysFont('Arial', 30)
-    font2 = pygame.font.SysFont('Arial', 70)
+    font = pygame.font.Font('nuevo_juego/fonts/game.otf',20)
+    font2 = pygame.font.Font('nuevo_juego/fonts/game.otf', 50)
     seleccion = None
 
     while seleccion is None:
-        screen.fill((30, 30, 30))
+        if fondo_imagen:
+            screen.blit(fondo_imagen,(0,0))
+        else :    
+            screen.fill((30, 30, 30))
         titulo1 = font2.render("¡Bienvenido al juego!", True, (255, 233, 255))
         titulo2= font.render("Selecciona tu modo de control", True, (255, 255, 255)) 
         titulo3 = font.render("¿Como queres moverte?", True, (255, 255, 255))
@@ -39,17 +52,27 @@ def menu_controles():
         titulo4 = font.render("Presiona [ESC] para salir", True, (200, 202, 200))   
         titulo5 = font.render("¡Para entrar al juego apreta [←][→]  SI queres moverte con las flechas!", True, (255, 233, 255))   
         titulo6 = font.render("¡Para entrar al juego apreta [A][D]  SI queres moverte con AD!", True, (255, 233, 255))
-
-        screen.blit(titulo1, (700, 20))
-        screen.blit(titulo2, (800, 100))
-        screen.blit(titulo3, (800, 150))
-        screen.blit(opcion1, (800, 200))
-        screen.blit(opcion2, (800, 250))
-        screen.blit(opcion4, (800, 320))
-        screen.blit(opcion6, (800, 380))  
-        screen.blit(titulo4, (800, 440))
-        screen.blit(titulo5, (590, 800))
-        screen.blit(titulo6, (590, 860))              
+        textos =[
+             (titulo1,  60),
+            (titulo2, 160),
+            (titulo3, 210),
+            (opcion1, 260),
+            (opcion2, 310),
+            (opcion4, 380),
+            (opcion6, 430),
+            (titulo4, 480),
+            (titulo5, screen.get_height() - 160),
+            (titulo6, screen.get_height() - 110),
+            
+        ]
+       
+        
+        for texto, y in textos:
+            rect = texto.get_rect(center=(screen.get_width()// 2, y))
+            rect.inflate_ip(20, 10)  # Margen alrededor del texto
+            pygame.draw.rect(screen, (0, 0, 0), rect, border_radius=12)
+            screen.blit(texto, rect.topleft)
+            
         pygame.display.flip()
 
         for evento in pygame.event.get():
@@ -104,19 +127,23 @@ def main():
     score = 0
     curacion_max = 1000
 
-    def score_total(frame, text, size, x, y):
+    def score_total(frame, text, size, x_ratio, y_ratio):
         font = pygame.font.SysFont('Small fonts', size, bold=True)
         text_frame = font.render(text, True, blanco, negro)
         text_rect = text_frame.get_rect()
-        text_rect.midtop = (1840, y-25)
+        width, height= frame.get_size()
+        text_rect.topleft = (int(width * x_ratio), int(height * y_ratio))
         frame.blit(text_frame, text_rect)
 
-    def barra_vida(frame, x, y, nivel):
-        longitud = 100
-        alto = 20
+    def barra_vida(frame, x_ratio, y_ratio, nivel):
+        width, height = frame.get_size()
+        longitud = int(width * 0.1)
+        alto = int(height * 0.03)
+        x=int(width * x_ratio)
+        y=int(height * y_ratio)
         fill = int((nivel/100)*longitud)
-        border = pygame.Rect(1770, 50, longitud, alto)
-        fill_rect = pygame.Rect(1770, 50, fill, alto)
+        border = pygame.Rect(x,y, longitud, alto)
+        fill_rect = pygame.Rect(x,y, fill, alto)
         pygame.draw.rect(frame, (255,0,55), fill_rect)
         pygame.draw.rect(frame, negro, border, 4)
 
@@ -314,7 +341,7 @@ def main():
             grupo_enemigos.add(nuevo_enemigo)
             explo = Explosion((enemigo.rect.centerx, enemigo.rect.centery - 25))
             grupo_explosiones.add(explo)
-            explosion_sonido.set_volume(0.3)
+            explosion_sonido.set_volume(0.077)
             explosion_sonido.play()
 
         colicion2 = pygame.sprite.spritecollide(jugador, grupo_balas_enemigos, True)
@@ -335,8 +362,8 @@ def main():
             if jugador.vida <= 0:
                 run = False
 
-        barra_vida(window, 50, 50, jugador.vida)
-        score_total(window, f"Puntuación: {score}", 30, 400, 50)
+        barra_vida(window, 0.03,0.04, jugador.vida)
+        score_total(window, f"Puntuación: {score}", 30,0.03,0.1)
         if score >= curacion_max:
             jugador.vida = 100 # Restablece la vida del jugador al máximo
             curacion_max += 1000 # Actualiza el próximo hito de puntuación 
@@ -349,9 +376,15 @@ def main():
 
 def mostrar_game_over(puntuacion):
     window = pygame.display.get_surface()
-    width, height = window.get_size()
-    window.fill((0, 0, 0))
-    font = pygame.font.SysFont('Arial', 40, bold=True)
+    width, height = window.get_size()   
+    try:
+        fondo_imagen = pygame.image.load('nuevo_juego/imagenes/fondo_princ.jpg')
+        fondo_imagen = pygame.transform.scale(fondo_imagen, (width, height))
+        window.blit(fondo_imagen, (0, 0))
+    except pygame.error:
+        window.fill((0, 0, 0))
+        
+    font = pygame.font.Font('nuevo_juego/fonts/game.otf', 30)
     texto1 = font.render("¡Perdiste! Bien jugado.", True, (255, 60, 60))
     texto2 = font.render(f"Puntuacion final: {puntuacion}", True, (255, 255, 255))
     texto3 = font.render("Presiona [R] para volver a jugar o [ESC] para salir", True, (200, 200, 200))
